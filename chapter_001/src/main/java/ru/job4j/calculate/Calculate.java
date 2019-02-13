48b4a05eb269ec4bd64429201d2ea2137776694e
r.paskov <rvk12@bk.ru>
2019-02-14 02:27:39 +0300

    1.3. JavaDoc. Code convention[#111926] 1.3.1 Maven Plugins[#111923]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+ * Class used for printing input parameter 3 times
+ * @author rvk12
+ * @since 13.02.2018
+ * @version 1
+ */
 public class Calculate {
 	public String echo(String value) {
 		return String.format("%s %s %s", value, value, value);
 	}
 	
+	/**
+	 * Creates new Calculate object and prints result of method echo in console
+	 * @param args - args
+	 */
 	public static void main(String[] args){
 		Calculate calc = new Calculate();
 		System.out.println(calc.echo("aah"));
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Roman (rvk12@bk.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
