9384c102fa1337be32296f94cb93433f79a52660
r.paskov <rvk12@bk.ru>
2019-02-14 23:48:43 +0300

    1.4. JUnit[#111927]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,20 +3,25 @@ package ru.job4j.calculate;
 /**
  * Class used for printing input parameter 3 times
  * @author rvk12
- * @since 13.02.2018
- * @version 1
+ * @since 0.1
+ * @version $Id$
  */
 public class Calculate {
-	public String echo(String value) {
-		return String.format("%s %s %s", value, value, value);
-	}
+
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 	
-	/**
-	 * Creates new Calculate object and prints result of method echo in console
-	 * @param args - args
-	 */
-	public static void main(String[] args){
-		Calculate calc = new Calculate();
-		System.out.println(calc.echo("aah"));
-	}
+	    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
