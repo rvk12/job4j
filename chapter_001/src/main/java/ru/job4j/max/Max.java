1df7b4eda82a456cba6b86591dab42e0b1336d2e
r.paskov <rvk12@bk.ru>
2019-02-22 00:19:54 +0300

    4.2. Максимум из двух чисел[#111890]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max;
+
+/**
+ * @author rvk12
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+
+    public int max(int first, int second){
+        return (first > second) ? first : second;
+    }
+
+}
