d9d821ebc7cdf9450e08c2fe658efd1ea1af21c1
r.paskov <rvk12@bk.ru>
2019-03-05 09:40:56 +0300

    6.0. Заполнить массив степенями чисел.[#111903]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * @author rvk12
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square {
+    /**
+     * Calculates square of numbers from 1 to bound.
+     * @param bound number in array on which calculation should be stopped.
+     * @return array.
+     * */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
+        for (int i = 1; i <= bound; i++) {
+            rst[i - 1] = (i) * (i);
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author rvk12
+ * @version $Id$
+ * @since 0.1
+ */
+public class SquareTest {
+
+    /**
+     * Test method when bound equals 3
+     * */
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+
+    /**
+     * Test method when bound equals 3
+     * */
+    @Test
+    public void whenBound5Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+
+    /**
+     * Test method when bound equals 3
+     * */
+    @Test
+    public void whenBound7Then14916253649() {
+        int bound = 7;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9, 16, 25, 36, 49};
+        assertThat(rst, is(expect));
+    }
+}
