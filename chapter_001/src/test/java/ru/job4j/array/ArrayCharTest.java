c3f495aca2737b3a5139c4bf49f3040c05e40869
r.paskov <rvk12@bk.ru>
2019-03-06 02:40:48 +0300

    6.4. Слово начинается с ...[#111906]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author rvk12
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayCharTest {
+
+    /**
+     * When string starts with prefix it should return tru
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Test string does not start with prefix it should return false
+     */
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
