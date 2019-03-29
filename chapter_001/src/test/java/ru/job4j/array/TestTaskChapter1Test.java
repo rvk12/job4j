efd6f29f29eec068e70341316da57b242f739a31
r.paskov <rvk12@bk.ru>
2019-03-30 01:05:42 +0300

    added more tests

--- chapter_001/src/test/java/ru/job4j/array/TestTaskChapter1Test.java
+++ chapter_001/src/test/java/ru/job4j/array/TestTaskChapter1Test.java
@@ -11,11 +11,51 @@ public class TestTaskChapter1Test {
      * check that result will contain values from both arrays and it is sorted.
      * */
     @Test
-    public void whenJoinTwoSortedArraysResultShouldBeSorted() {
+    public void whenJoinTwoSortedArraysSecondIsBiggerResultShouldBeSorted() {
         TestTaskChapter1 finalArray = new TestTaskChapter1();
         int[] arrayFirst = {1, 3, 10};
         int[] arraySecond = {3, 12, 14, 19, 34};
         int[] expect = {1, 3, 3, 10, 12, 14, 19, 34};
+        int[] result = finalArray.merge(arrayFirst, arraySecond);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenJoinTwoSortedArraysFirstIsBiggerResultShouldBeSorted() {
+        TestTaskChapter1 finalArray = new TestTaskChapter1();
+        int[] arrayFirst = {1, 3, 10, 12, 23, 57, 90, 100, 700};
+        int[] arraySecond = {3, 12, 14, 19, 34};
+        int[] expect = {1, 3, 3, 10, 12, 12, 14, 19, 23, 34, 57, 90, 100, 700};
+        int[] result = finalArray.merge(arrayFirst, arraySecond);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenJoinTwoEqualSortedArraysResultShouldBeSorted() {
+        TestTaskChapter1 finalArray = new TestTaskChapter1();
+        int[] arrayFirst = {3, 12, 14, 19, 34};
+        int[] arraySecond = {3, 12, 14, 19, 34};
+        int[] expect = {3, 3, 12, 12, 14, 14, 19, 19, 34, 34};
+        int[] result = finalArray.merge(arrayFirst, arraySecond);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenJoinFirstEmptyArraySecondNotEmptySortedResultShouldBeSorted() {
+        TestTaskChapter1 finalArray = new TestTaskChapter1();
+        int[] arrayFirst = {};
+        int[] arraySecond = {3, 12, 14, 19, 34};
+        int[] expect = {3, 12, 14, 19, 34};
+        int[] result = finalArray.merge(arrayFirst, arraySecond);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenJoinFirstNotEmptySortedArraySecondEmptyResultShouldBeSorted() {
+        TestTaskChapter1 finalArray = new TestTaskChapter1();
+        int[] arrayFirst = {3, 12, 14, 19, 34};
+        int[] arraySecond = {};
+        int[] expect = {3, 12, 14, 19, 34};
         int[] result = finalArray.merge(arrayFirst, arraySecond);
         assertThat(result, is(expect));
     }
