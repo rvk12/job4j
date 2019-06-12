11e86d7d071cac332367c638769dc39cc48f6e6b
r.paskov <rvk12@bk.ru>
2019-06-12 21:09:41 +0300

    3. if-else-throw[#111970]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -18,11 +18,10 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range.");
         }
+        return key;
     }
 
 
