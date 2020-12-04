84d448019238d45cf2f64a01b69c437e5f7bd7d2
Roman <rvk12@bk.ru>
2020-12-04 10:55:30 +0300

    110062_111855

---
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,10 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    public List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(p -> p.getAddress()).collect(Collectors.toList());
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenCollectReturnAddresses() {
+        List<Profile> profiles = new ArrayList<Profile>();
+        profiles.add(new Profile(new Address("Orsk","Lenin",5,23)));
+        profiles.add(new Profile(new Address("Penza","Mira",41,23)));
+        List<Address> expected = Arrays.asList(new Address("Orsk", "Lenin", 5, 23)
+                , new Address("Penza","Mira",41,23));
+        assertThat(new Profiles().collect(profiles), is(expected));
+    }
+}
